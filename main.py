39a2e10e020f467b2bf258b6835d5597f182cf7d
Ubuntu <akmalmasud96@DeepLearningVM.0en1yjqjpyculhztkoecuubycd.bx.internal.cloudapp.net>
2020-09-13 18:44:53 +0000

    Create new Genrater

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     num_epochs = 10
     batch_size = 20
      
-    words_to_index, index_to_words, word_to_vec_map = read_glove_file("./inputs/glove.6B.100d.txt")
+    words_to_index, index_to_words, word_to_vec_map = read_glove_file("./inputs/glove.6B.50d.txt")
 
     with open('./inputs/train.txt', 'r') as f:
         train_data = json.loads(f.read())
@@ -20,14 +20,14 @@ if __name__ == "__main__":
     with open('./inputs/test.txt', 'r') as f:
         test_data = json.loads(f.read())
     # getting max length comment from comments corpus
-    maxLen = 100
+    maxLen = 50
     
-    train_data_indices, y_train_indices= sentences_to_indices ( np.array(train_data[:10]), word_to_index = words_to_index, max_len =maxLen, vocabulary=len(words_to_index))
-    test_data_indices, y_test_indices= sentences_to_indices ( np.array(test_data[:10]), word_to_index = words_to_index, max_len =maxLen, vocabulary=len(words_to_index))
+    train_data_indices, y_train_indices= sentences_to_indices ( np.array(train_data), word_to_index = words_to_index, max_len =maxLen, vocabulary=len(words_to_index))
+    test_data_indices, y_test_indices= sentences_to_indices ( np.array(test_data), word_to_index = words_to_index, max_len =maxLen, vocabulary=len(words_to_index))
 
     #create model 
     model = My_Model((maxLen,), word_to_vec_map, words_to_index,  hidden_size = 128)
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['categorical_accuracy'])
-    checkpointer = ModelCheckpoint(filepath='./model-{epoch:02d}.hdf5', verbose=1)
+    checkpointer = ModelCheckpoint(filepath='./models/model-{epoch:02d}.hdf5', verbose=1)
     
     model.fit(train_data_indices, y_train_indices, epochs = 5, batch_size = 10,validation_data=(test_data_indices, y_test_indices), shuffle=True, callbacks=[checkpointer])
--- src/dataset.py
+++ src/dataset.py
@@ -48,3 +48,52 @@ def sentences_to_indices(X, word_to_index, max_len, vocabulary):
         y_indices[i, :, :] = to_categorical(temp_y, num_classes=vocabulary)
     
     return X_indices, y_indices
+
+
+class KerasBatchGenerator(object):
+    def __init__(self, data, batch_size, vocabulary,word_to_index, max_len ):
+        self.data = data
+        self.batch_size = batch_size
+        self.vocabulary = vocabulary
+        self.word_to_index = word_to_index
+        self.max_len = max_len
+
+        # this will track the progress of the batches sequentially through the
+        # data set - once the data reaches the end of the data set it will reset
+        # back to zero
+        self.current_idx = 0
+
+
+    def generate(self):
+        x = np.zeros((self.batch_size, self.max_len))
+        y = np.zeros((self.batch_size, self.max_len, self.vocabulary))
+        while True:
+            for i in range(self.batch_size):
+                if self.current_idx >= len(self.data):
+                    # reset the index back to the start of the data set
+                    self.current_idx = 0
+                
+                # Convert the ith training sentence in lower case and split is into words. You should get a list of words.
+                sentence_words = self.data[self.current_idx].lower().split()
+
+                # Initialize j to 0
+                j = 0
+
+                # Loop over the words of sentence_words
+                for w in sentence_words:
+                    # Set the (i,j)th entry of X_indices to the index of the correct word.
+                    x[i, j] = self.word_to_index.get(w, self.word_to_index["unk"])
+                    # Increment j to j + 1
+                    j = j+1
+
+                    if j >= self.max_len:
+                        break
+            
+                temp_y = x[i,1:]
+                # making array equal to max size
+                temp_y = np.append(temp_y,0)
+                # convert all of temp_y into a one hot representation
+                y[i, :, :] = to_categorical(temp_y, num_classes=self.vocabulary)
+                self.current_idx += 1
+            yield x, y
+    
